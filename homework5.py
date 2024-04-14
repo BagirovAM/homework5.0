3d74538d77f45d67148572f57b59400a5d092ea3
BagirovAM <166054204+BagirovAM@users.noreply.github.com>
2024-04-14 11:08:18 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ["apple", "banana", "lemon", "cherry", "clock"]
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = "table"
+print(my_list)
+
+my_dict = {"apple": "яблоко", "banana": "яблоко", "lemon": "лимон"}
+print(my_dict)
+print(my_dict["apple"])
+my_dict["apple"] = 322
+print(my_dict)
